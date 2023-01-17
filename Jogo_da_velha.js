74e2c5db50a274696d9b5536c594df5d15509a7e
Vanessa Santos <114835013+VanessaPro@users.noreply.github.com>
2023-01-17 15:14:23 -0300

    Feito correção em const

--- Jogo_da_velha.js
+++ Jogo_da_velha.js
@@ -1,11 +1,4 @@
 
-const cellElements = document.querySelectorAll("[data-cell]");
-const board = document.querySelector("[data-board]");
-const winningMessageTextElement = document.querySelector(
-  "[data-winning-message-text]"
-);
-const winningMessage = document.querySelector("[data-winning-message]");
-const restartButton = document.querySelector("[data-restart-button]")  
 
 const Jogo_da_velha = {
  
